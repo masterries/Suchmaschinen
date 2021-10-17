75055e73ba39775f2b75af77ca74c09d8bd47b2b
masterries <26522122+masterries@users.noreply.github.com>
2021-10-17 16:23:06 +0200

    Update Backend Sorting+Filter

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ app.set('port', process.env.PORT || 5050 );
 app.use(cors());
 app.use(bodyParser.json());
 
+
+
 const client = new elasticsearch.Client({
     host: 'http://node-1.hska.io:9200',
     log: 'error'
@@ -26,24 +28,65 @@ app.get('/search', function (req, res){
     
 
 
-    let room = req.query['q']
+    let name = req.query['q']
     let order = req.query['order']
     let by1 = req.query['by']
-
-
-    if(by1 == "videos"){
-        searchVideo(res,order,room)
+        
+      // let requestBody =  searchBasic("title",name)
+      // requestBody = searchSort(requestBody,order,by1);
+      // sendBody(res,requestBody,true);
       
-    }else{
+      searchSortNormal(res,"title",name,order,by1);
 
-        searchFollowers(res,order,room)
-      
-    }
    
     
   })
 
-  function searchVideo(res,order,room){
+  function searchSortNormal(res,category,name,order,by1){
+    const requestBody = new esb.RequestBodySearch()
+    .query(new esb.MatchQuery(category, name)).sort(new esb.sort(by1, order));
+
+    client.search({index: "youtubechannel", body: requestBody.toJSON()}).then(results => {
+        if(results.hits.total.value==0 ){
+            searchSortFuzzy(res,category,name,order,by1);
+        }else{
+            console.log(results)
+            res.send(results.hits.hits);
+
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+    });
+
+
+
+
+
+
+  }
+
+
+  function searchSortFuzzy(res,category,name,order,by1){
+    const requestBody = new esb.RequestBodySearch()
+    .query(new esb.FuzzyQuery(category, name)).sort(new esb.sort(by1, order));
+
+
+    client.search({index: "youtubechannel", body: requestBody.toJSON()}).then(results => {
+        if(results.hits.total.value==0 ){
+            fuzzySend(res,requestBody);
+        }else{
+            console.log(results)
+            res.send(results.hits.hits);
+
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+    });
+  }
+
+  function searchVideo(order,room){
     client.search({
         index: "youtubechannel",
         body: {
@@ -72,78 +115,55 @@ app.get('/search', function (req, res){
     });
   }
 
-  function searchFollowers(res,order,room){
+  function searchBasic(category,name){
     const requestBody = new esb.RequestBodySearch()
-            .query(new esb.MatchQuery('category_name', 'Music'));
-            console.log(requestBody.toJSON())
-            client.search({index: "youtubechannel", body: requestBody.toJSON()}).then(results => {
-                if(results.hits.total.value==0){
-                    fuzzyVideo(res,order,room);
-                }else{
-                    console.log(results)
-                    res.send(results.hits.hits);
-        
-                }
-        
-         
-                
-            })
-            .catch(err=>{
-                console.log(err)
-            });
-      
-   
+            .query(new esb.MatchQuery(category, name));
+            return requestBody;
+  }
+
+  function searchSort(requestBody,order,by1){
+      if(order == "normal"){
+          return requestBody;
+      }
+    const requestBody1 = requestBody.sort(new esb.sort(by1, order));
+    return requestBody1;
+            
   }
 
 
-  function fuzzyVideo(res,order,room){
+function sendBody(res,requestBody,fuzzySend1){
+    client.search({index: "youtubechannel", body: requestBody.toJSON()}).then(results => {
+        if(results.hits.total.value==0 && fuzzySend1 == true){
+            fuzzySend(res,requestBody);
+        }else{
+            console.log(results)
+            res.send(results.hits.hits);
 
-    client.search()
-   
-    client.search({
-        index: "youtubechannel",
-        body: {
-                //Variable einsetzen 
-            sort: { "videos" : { "order": order } },
-            
-            query: {
-                fuzzy: {"title":room}
-            }
         }
     })
-    .then(results => {
-        console.log(results)
-            res.send(results.hits.hits);
-        
-    })
     .catch(err=>{
-      console.log(err)
-      res.send([]);
+        console.log(err)
     });
+}
+
+function fuzzySend(res,requestBody){
+    console.log(requestBody.getDSL);
+    console.log(requestBody.body)
+    client.search({index: "youtubechannel",updatedJson}).then(results => {
+        
+            console.log(results)
+            res.send(results.hits.hits);
+
+        
+    });
+
+}
+
+function renameKey ( obj, oldKey, newKey ) {
+    obj[newKey] = obj[oldKey];
+    delete obj[oldKey];
   }
 
-  function fuzzyFollowers(res,order,room){
-    client.search({
-        index: "youtubechannel",
-        body: {
-
-            sort: { "followers" : { "order": order } },
-            
-            query: {
-                fuzzy: {"title":room}
-            }
-        }
-    })
-    .then(results => {
-        console.log(results)
-            res.send(results.hits.hits);
-        
-    })
-    .catch(err=>{
-      console.log(err)
-      res.send([]);
-    });
-  }
 
 app.listen(app.get('port'), function() {
     console.log('Your node.js server is running on PORT: ',app.get('port'));
